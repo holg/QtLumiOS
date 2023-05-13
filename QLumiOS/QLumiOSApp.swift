56707debdb20b9e532faebf865f435befcb043de
Holger Trahe <trahe@mac.com>
2023-05-13 20:35:43 +0200

    Initial Commit

---
+++ QLumiOS/QLumiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  QLumiOSApp.swift
+//  QLumiOS
+//
+//  Created by Holger Trahe on 13.05.23.
+//
+
+import SwiftUI
+
+@main
+struct QLumiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
